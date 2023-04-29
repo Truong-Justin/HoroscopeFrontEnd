5b055140282ca00043c929099de46951495273c0
Justin Truong <satruongjustin@gmail.com>
2023-04-28 21:53:39 -0500

    initial commit

---
+++ fetch.js
@@ -0,0 +1,15 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+//creates and returns a response object using fetch
+//and prints the JSON data within the response object
+async function getData() {
+    const response = await fetch('https://horoscopeapi-v6vga.ondigitalocean.app/api/get-chineseHoroscope/daily?animal=dragon&day=today');
+    const data = await response.json();
+    console.log(data['data']);
+}
+
+function changeParagraph(message) {
+    document.getElementById('paragraph').innerText = "Message changed";
+}
+
+getData();
